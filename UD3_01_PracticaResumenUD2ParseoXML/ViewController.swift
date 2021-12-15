33976c1569baea3918403c5dd8ed2c9cdc41154d
Marta M Aguilera <73697174+martamagui@users.noreply.github.com>
2021-12-15 11:24:38 +0100

    Initial Commit

---
+++ UD3_01_PracticaResumenUD2ParseoXML/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UD3_01_PracticaResumenUD2ParseoXML
+//
+//  Created by Marta Molina Aguilera on 15/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
