f300c3baba2ac74bd95e3604e7685b93f82bb0e1
Marta M Aguilera <73697174+martamagui@users.noreply.github.com>
2021-12-20 09:47:49 +0100

    More comments

--- UD3_01_PracticaResumenUD2ParseoXML/VCDetalle2.swift
+++ UD3_01_PracticaResumenUD2ParseoXML/VCDetalle2.swift
@@ -16,10 +16,12 @@ class VCDetalle2: UIViewController, WKNavigationDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Configura el cargar el contenido de la vista.
         configureView()
     }
     
     private func configureView(){
+        //Cambia el String recibido por el segue a Url y carga la petición en la webView
         guard let urlRecibida = contenidoWeb else { return }
         guard let url = URL(string: urlRecibida) else {return}
         print(url)
@@ -28,11 +30,11 @@ class VCDetalle2: UIViewController, WKNavigationDelegate {
         webview.navigationDelegate = self
     }
     
-    //métodos de navigation delegate
+    //Ejecuta la animación del ActivityIndicatorView
     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
         loadAnim.startAnimating()
     }
-    
+    //Una vez ha carado el contenido de la web pausa la animación y esconde la ruedecita
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         loadAnim.stopAnimating()
         loadAnim.hidesWhenStopped = true
--- UD3_01_PracticaResumenUD2ParseoXML/VCFeed.swift
+++ UD3_01_PracticaResumenUD2ParseoXML/VCFeed.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class VCFeed:  UITableViewController, XMLParserDelegate {
-   
+    
     @IBOutlet var miTabla: UITableView!
     
     var tematica = String()
@@ -23,18 +23,23 @@ class VCFeed:  UITableViewController, XMLParserDelegate {
     var parser: XMLParser = XMLParser()
     let urlXML="https://www.muyinteresante.es/rss"
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //Ejecuto todo lo relativo al parseo del XML
+        prepararParser()
+    }
+    
+    //Parseo de categorías
+    //Preparo el parseo y le asigno el URL
+    func prepararParser(){
+        //Cambia el String de urlXML a Url y carga la petición en la webView
         guard let url = URL(string: urlXML) else {return}
         guard let parser = XMLParser(contentsOf: url) else { return}
         parser.delegate = self
         parser.parse()
-        
     }
     
+    //Inicia los elemntos que van a ser parseados del XML si el nombre del elemento equivale a "Item"
     func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
         nombreElemento = elementName
         if nombreElemento == "item"
@@ -48,6 +53,7 @@ class VCFeed:  UITableViewController, XMLParserDelegate {
         }
     }
     
+    //Lee cada elemento del XML y va añadiendo cada caracter a su correspondiente String
     func parser(_ parser: XMLParser, foundCharacters string: String) {
         let caracter = string.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
         if !caracter.isEmpty
@@ -55,29 +61,43 @@ class VCFeed:  UITableViewController, XMLParserDelegate {
             if nombreElemento == "title"
             {
                 titulo += caracter
-            }else if nombreElemento == "link"{
+            }
+            else if nombreElemento == "link"
+            {
                 link += caracter
-            }else if nombreElemento == "description"{
+            }
+            else if nombreElemento == "description"
+            {
                 descripcion += caracter
-            }else if nombreElemento == "category"{
+            }
+            else if nombreElemento == "category"
+            {
                 category += caracter
-            }else if nombreElemento == "pubDate"{
+            }
+            else if nombreElemento == "pubDate"
+            {
                 pubDate += caracter
             }
         }
     }
-   // Cuando termina el elemento
+   /*Cuando termina el elemento una vez termina de leer cada item,
+     si coincide la categoría con la indicada en la anterior pantalla
+     lo añade a la lista de la tabla. Si no, no.
+     */
     func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
         if (elementName == "item")
         {
             if tematica == "Todas"{
                 annadirElemento()
-            }else if tematica == category{
+            }
+            else if tematica == category
+            {
                 annadirElemento()
             }
         }
     }
     
+    //Función que forma una estructura "item" y lo añade a la lista de items
     func annadirElemento(){
         let arrProvisional = descripcion.components(separatedBy: "\"/>\n    \n                        ")
         descripcion = "\(arrProvisional[1])"
@@ -91,23 +111,32 @@ class VCFeed:  UITableViewController, XMLParserDelegate {
     }
     
     // MARK: - Table view data source
+    //Indica a la tabla que sólo hay una única sección
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
+    //La tabla tendrá tantos elementos como sea el tamaño de la lista de Items
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemList.count
     }
-
+    
+    // Añade el título de la noticia a la tabla.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Configure the cell...
         let cell =  UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
         cell.textLabel?.text = itemList[indexPath.row].title
         return cell
     }
+    
+    //Segue ejecutada al pulsar sobre un elemento de la lista.
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "irADetalle", sender: nil)
     }
+    
+    /*Al pulsar sobre una de las celdas toma el elemento seleccionado
+     y pasa ese item por parámetro a la vista de detalle.
+     */
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "irADetalle"
         {
